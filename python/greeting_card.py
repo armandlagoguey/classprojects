cb3e69dafd7ff325a74ffd7918c8ae3b3d82adf4
Armand Lagoguey <lagogueyarmand@gmail.com>
2025-09-29 15:07:05 +0200

    Create greeting_card.py

---
+++ python/greeting_card.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+your_name=input("Enter your Name: ")
+recipient_name=input("Enter the recipient's name: ")
+message="Happy birthday !"
+
+print('-'*20,f"\nDear {recipient_name}\n{message}\nFrom,\n{your_name}\n",'-'*20)
